574c5c59dbfd1928b3ae8da88d41b63e4750d020
Dmitriy <dnechoroshev@gmail.com>
2023-05-26 22:37:10 +0300

    Initial commit

---
+++ src/main/java/ru/dnechoroshev/paymentsservice/PaymentsServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.dnechoroshev.paymentsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentsServiceApplication.class, args);
+    }
+
+}
